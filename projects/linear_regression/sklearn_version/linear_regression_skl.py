bdd8912fb39f9a58fc587b13e14d46e0e157de32
Olabode Zacchaeus <zacchaeusoluwasayo@gmail.com>
2026-08-03 15:38:18 +0100

    Implement RMSE calculation for linear regression model
    Added calculation and display of RMSE for model evaluation.

--- projects/linear_regression/sklearn_version/linear_regression_skl.py
+++ projects/linear_regression/sklearn_version/linear_regression_skl.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 
 #load in data
 df = pd.read_csv("housing.csv")
@@ -30,6 +31,10 @@ X_train, X_test, y_train, y_test = train_test_split(X, y)
 model = LinearRegression()
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
+
+#RMSE
+rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+print(f"RMSE: ${rmse:,.2f}")
 
 #plotting
 plt.scatter(y_test, y_pred, alpha=0.3)
